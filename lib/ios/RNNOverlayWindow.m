733398f9fc6c56f447f03d6acda101bc4c2ade83
Javier <javier.morant84@gmail.com>
2020-04-09 10:12:44 +0200

    Fix iPad issue

--- lib/ios/RNNOverlayWindow.m
+++ lib/ios/RNNOverlayWindow.m
@@ -4,10 +4,10 @@
 
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
 	UIView *hitTestResult = [super hitTest:point withEvent:event];
-	
-	if ([hitTestResult isKindOfClass:[UIWindow class]]) {
-		return nil;
-	}
+
+	if ( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad && [[hitTestResult class] isEqual:[UIView class]]) {
+        return nil;
+    }
 	
 	return hitTestResult;
 }
